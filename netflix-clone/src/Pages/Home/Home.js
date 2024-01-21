670177b93f11ae990f8715be8950cf1b90d08020
tesfa-hub <84892230+tesfa-hub@users.noreply.github.com>
2024-01-21 15:13:53 -0600

    plus

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from './../../Components/Header/Header';
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  
+  )
+}
+
+export default Home
+
