e1409c4817f0df27d1d5ac99d5af3fb49d0ca452
tesfa-hub <84892230+tesfa-hub@users.noreply.github.com>
2024-01-22 14:56:57 -0600

    icons added

---
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+ <FacebookOutlinedIcon/>
+    <InstagramIcon/>
+    <YouTubeIcon/>
+    </div>
+   
+  )
+}
+
+export default Footer
--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import "./header.css"
 const Header = () => {
   return (
-    <div>Header</div>
+    
+      <div>
+         <SearchIcon/>
+         <NotificationsNoneIcon/>
+         <AccountBoxIcon/>
+         <ArrowDropDownIcon/>
+      </div>
+         
+        
   )
 }
 
